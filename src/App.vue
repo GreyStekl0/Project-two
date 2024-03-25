c4213c350f8598c49070fff6e2f5a74c837b7f18
Stekl0 <gulev_s@list.ru>
2024-03-25 19:14:08 +0500

    project in progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 
+import VHeader from "@/components/VHeader.vue";
+import VPromo from "@/components/VPromo.vue";
+import VClass from "@/components/VClass.vue";
 </script>
 
 <template>
-
+<v-header />
+  <main>
+  <v-promo />
+  <v-class />
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/VHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <nav class="navigation">
+      <div class="container">
+        <div class="navigation__wrapp">
+          <img src="/logo.svg" alt="logo">
+          <ul class="navigation__list">
+            <li class="navigation__list-item">
+              <p> Возможности </p>
+            </li>
+            <li class="navigation__list-item">
+              <p> Как это работает? </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
